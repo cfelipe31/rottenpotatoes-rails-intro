f1b795335a738dea6b68ea825fc04dd77583cec5
Hugo <hugombarreto@gmail.com>
2015-11-13 03:07:32 +0000

    Adding Color

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort_by = params[:sort_by]
-    @movies = Movie.order(sort_by)
+    @sort_by = params[:sort_by]
+    @movies = Movie.order(@sort_by)
   end
 
   def new
